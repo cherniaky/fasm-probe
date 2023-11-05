0b647569120547a6b33b35129eef2d48283b3926
CherniakYura <juracherniak@gmail.com>
2023-11-05 11:58:42 +0100

    Add a bunch of sys calls

--- hello.asm
+++ hello.asm
@@ -1,18 +1,95 @@
 format ELF64 executable
 
-macro write fd, buf, count 
+SYS_write equ 1
+SYS_exit equ 60
+SYS_socket equ 41
+SYS_accept equ 43
+SYS_bind equ 49
+SYS_listen equ 50
+SYS_close equ 3
+
+AF_INET equ 2
+SOCK_STREAM equ 1
+INADDR_ANY equ 0
+
+STDOUT equ 1
+STDERR equ 2
+
+EXIT_SUCCESS equ 0
+EXIT_FAILURE equ 1
+
+MAX_CONN equ 5
+
+macro syscall1 number, a
 {
-    mov rax, 1
+    mov rax, number
+    mov rdi, a
+    syscall
+}
+
+macro syscall2 number, a, b
+{
+    mov rax, number
+    mov rdi, a
+    mov rsi, b
+    syscall
+}
+
+macro syscall3 number, a, b, c
+{
+    mov rax, number
+    mov rdi, a
+    mov rsi, b
+    mov rdx, c
+    syscall
+}
+
+macro write fd, buf, count
+{
+    mov rax, SYS_write
     mov rdi, fd
     mov rsi, buf
     mov rdx, count
     syscall
 }
 
-macro exit status 
+macro close fd
 {
-    mov rax, 60
-    mov rdi, status
+    syscall1 SYS_close, fd
+}
+
+;; int socket(int domain, int type, int protocol);
+macro socket domain, type, protocol
+{
+    mov rax, SYS_socket
+    mov rdi, domain
+    mov rsi, type
+    mov rdx, protocol
+    syscall
+}
+
+;; int bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
+macro bind sockfd, addr, addrlen
+{
+    syscall3 SYS_bind, sockfd, addr, addrlen
+}
+
+;;       int listen(int sockfd, int backlog);
+macro listen sockfd, backlog
+{
+    syscall2 SYS_listen, sockfd, backlog
+}
+
+;; int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
+macro accept sockfd, addr, addrlen
+{
+    syscall3 SYS_accept, sockfd, addr, addrlen
+}
+
+macro exit code
+{
+    mov rax, SYS_exit
+    mov rdi, code
     syscall
 }
 
